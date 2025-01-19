93c603ce5321bbcdca4800905e413b189c36ce3d
Steven Wickers <stevenwickers@Stevens-MacBook-Pro-2.local>
2025-01-19 16:08:59 -0500

    add .env file and updaetd esling settings

--- eslint.config.js
+++ eslint.config.js
@@ -11,26 +11,3 @@ module.exports = {
     'comma-spacing': [ 'warn', { 'before': false, 'after': true }],
   },
 };
-
-
-/*
-module.exports = {
-  languageOptions: {
-    globals: {
-      node: true,
-      es2021: true,
-    }
-  },
-  extends: 'eslint:recommended', // Use recommended rules
-  parserOptions: {
-    ecmaVersion: 12, // Set the ECMAScript version
-    sourceType: 'module', // Use modules
-  },
-  rules: {
-    semi: [ 'warn', 'always' ],
-    quotes: [ 'error', 'single' ],
-    indent: [ 'warn', 2, { SwitchCase: 1 }],
-    'no-trailing-spaces': 'warn',
-    'comma-spacing': [ 'warn', { 'before': false, 'after': true }],
-  },
-};*/
